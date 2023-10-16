ff271b39881a705ca3b9c4c4cbf95f1226e82750
davidchau21 <davidduongxu1@gmail.com>
2023-10-16 21:36:00 +0700

    update lab3

--- Lab3-figma/App.js
+++ Lab3-figma/App.js
@@ -1,34 +1,22 @@
+import { useState, createContext, useContext } from 'react';
+import {NavigationContainer} from "@react-navigation/native"
+import {createStackNavigator} from "@react-navigation/stack"
+import screen1  from "./screen/screen_1"
+import screen2 from "./screen/screen_2"
 
-import { StyleSheet, Text, Image, View } from 'react-native';
+let Stack = createStackNavigator();
+export let MyContext = createContext();
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      
-      <View style={{ flex: 0.3 }}></View>
-      <View style={{ flex: 3 }}>
-        <Image style={{ flex: 1, height: '100%', width: 'auto', resizeMode: 'center' }} source={{ uri: 'https://www.xtmobile.vn/vnt_upload/product/08_2019/vsmart-joy-3-2gb-32gb.jpg'}}></Image>
-      </View>
-      <Text style={{ flex: '0 1 0%', fontSize:15, alignItems:'center', left: '20px'}}>Điện Thoại Vsmart Joy 3 - Hàng chính hãng</Text>
-      <View style={{ flex: 1, flexDirection: 'row'}}>
-        <Image style={{ flex: 1, height: '50%', width: 'auto', left:0}} source={{uri:'https://www.pngmart.com/files/23/Stars-PNG.png'}}></Image>
-        <Text style={{flex:1, justifyContent:'center', fontSize:16, alignSelf:'center', top: '-29px', left: '10px'}}>(Xem 823 danh gia)</Text>
-      </View>
-      <View style={{flex:1, flexDirection:'row'}}>
-        <Text style={{flex:1, fontWeight:'bold', fontSize:20, left: 0}}>1.790.000 đ</Text>
-        <Text style={{ flex: 1, fontSize: 18, fontSize:16, alignSelf:'center', top: '-29px', left: '10px'}}><del>1.790.000 đ</del></Text>
-      </View>
-
-    </View>
-    
+export default function App(){
+  let [img, setImg] = useState('https://24hstore.vn/images/products/2021/04/01/large/vsmart-live-4-black.jpg')
+  return(
+    <MyContext.Provider value={{ img , setImg}}>
+      <NavigationContainer>
+        <Stack.Navigator>
+          <Stack.Screen name="screen1" component={screen1} options={{ headerShown: false }} />
+          <Stack.Screen name="screen2" component={screen2} options={{ headerShown: false }} />
+        </Stack.Navigator>
+      </NavigationContainer>
+    </MyContext.Provider>
   );
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    paddingLeft: '5%',
-    paddingRight: '5%',
-  },
-});
+} 
