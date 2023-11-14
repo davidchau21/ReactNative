c650af09ee38fbb4ad8c58129f452c63dbbf85a8
davidchau21 <davidduongxu1@gmail.com>
2023-11-14 14:54:00 +0700

    fix lab7

--- Lab7-CafeWord/screen/screen1.js
+++ Lab7-CafeWord/screen/screen1.js
@@ -1,7 +1,9 @@
 import { StyleSheet, Text, View, Image, TouchableOpacity } from 'react-native';
+import { useNavigation } from "@react-navigation/native";
 
 
 export default function App() {
+  let navigation = useNavigation();
   
   return (
     <View style={styles.container}>
@@ -9,21 +11,25 @@ export default function App() {
       <View style={{flex:1, margin:15, alignItems:"center"}}>
       <Text style={{ alignItems:"center", justifyContent:"space-between", padding:40, fontSize:20, fontWeight:'bold'}}>Welcome to Cafe world</Text>
         <Image 
-        source={{uri: "https://media-cdn.tripadvisor.com/media/photo-s/11/db/5e/d4/vidriera.jpg"}}
+        source={require('../assets/hinh1.png')}
         style={{height:70,width:200,  borderRadius:15, margin:20}}
         ></Image>
         
         <Image 
-        source={{uri: "https://media-cdn.tripadvisor.com/media/photo-s/11/db/5e/d4/vidriera.jpg"}}
+        source={require('../assets/hinh2.png')}
         style={{height:70,width:200, borderRadius:15, margin:20}}
         ></Image>
         <Image 
-        source={{uri: "https://media-cdn.tripadvisor.com/media/photo-s/11/db/5e/d4/vidriera.jpg"}}
+        source={require('../assets/hinh3.jpg')}
         style={{height:70,width:200, borderRadius:15, margin:20}}
         ></Image>
       </View>
       <View style={{flex:0.2}}>
-        <TouchableOpacity style={{height:50,width:280, justifyContent: 'center', alignItems: 'center', backgroundColor: '#00BDD6', borderRadius: 5 }}>
+        <TouchableOpacity style={{height:50,width:280, justifyContent: 'center', alignItems: 'center', backgroundColor: '#00BDD6', borderRadius: 5 }}
+        onPress={() => {
+          navigation.navigate('Screen2');
+        }} 
+        >
             <Text>GET STARTED</Text>
         </TouchableOpacity>
       </View>
--- Lab7-CafeWord/screen/screen2.js
+++ Lab7-CafeWord/screen/screen2.js
@@ -1,14 +1,114 @@
-import { StyleSheet, Text, View } from 'react-native'
+import { StyleSheet, Text, View, Pressable, FlatList, Image } from 'react-native'
 import React from 'react'
 
-const screen2 = () => {
+export default function App() {
+
   return (
-    <View>
-      <Text>screen2</Text>
+    <View style={styles.container}>
+      <View style={styles.viewTitle}>
+        <Pressable onPress={()=>navigation.navigate('Screen01')}>
+          <Image
+            source={require("../assets/BackIcon.png")}
+            style={{width:20,height:50}}
+          />
+        </Pressable>
+        <Text style={{fontWeight:700, fontSize:24, marginRight:20}}>
+          Shop Nears Me
+        </Text>
+        <Image
+          source={require("../assets/SearchIcon.png")}
+          resizeMode="contain"
+          style={{width:25,height:50}}
+        />
+      </View>
+      {/* <FlatList
+        data={data}
+        renderItem={({item})=>
+        <Pressable style={styles.viewShop} onPress={()=>navigation.navigate("CafeScreen3", { item })}>
+          <Image
+            source={{uri:item.img}}
+            resizeMode="cover"
+            style={{width:310,height:120,borderRadius:6}}
+          />
+          <View style={{flexDirection:"row",marginTop:5}}>
+            <View style={{flexDirection:"row", width:280}}>
+              <View style={styles.viewOpen}>
+                <Image
+                  source={{uri:item.imgState}}
+                  resizeMode="contain"
+                  style={{width:20,height:20}}
+                />
+                <Text style={{fontWeight:400, fontSize:14, marginLeft:5}}>{item.state}</Text>
+              </View>
+              <View style={styles.viewTime}>
+                <Image
+                  source={require("../assets/ClockIcon.png")}
+                  resizeMode="contain"
+                  style={{width:20,height:20}}
+                />
+                <Text style={{fontWeight:400, fontSize:14, marginLeft:5, color:"red"}}>{item.time}</Text>
+              </View>
+            </View>
+              <Image
+                source={require("../assets/MapIcon.png")}
+                resizeMode="contain"
+                style={{width:20,height:20, marginLeft:2}}
+              />
+          </View>
+          <Text style={styles.txtNameShop}>{item.nameShop}</Text>
+          <Text style={styles.txtAdress}>{item.address}</Text>
+        </Pressable>
+      }
+      /> */}
+      
     </View>
   )
 }
 
-export default screen2
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    backgroundColor:"rgba(18, 15, 40, 0.12)"
+  },
+  viewTitle: {
+    flexDirection: "row",
+    alignItems:"center",
+    gap:30
+  },
+  viewShop:{
+    marginTop:15,
+    width:310,
+    height:190,
+    borderRadius:6,
+    backgroundColor:"#fff",
+    shadowOpacity: 0.3,
+      shadowOffset: {
+	    width: 0,
+	    height: 4,
+      },
+    shadowColor: '#000'
+  },
+  viewOpen:{
+    marginLeft:2,
+    flexDirection:"row",
+    backgroundColor:"#F3F4F6"
+  },
+  viewTime:{
+    marginLeft:10,
+    flexDirection:"row",
+    backgroundColor:"#F3F4F6"
+  },
+  txtNameShop:{
+    marginLeft:5,
+    fontWeight:700,
+    fontSize:16
+  },
+  txtAdress:{
+    marginLeft:5,
+    fontWeight:400,
+    fontSize:14,
+    color:"grey"
+  }
+});
